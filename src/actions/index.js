50eae9331bcb614ffb4a03df06f5a52bfe3ffd0f
Haz-git <zhouharry1@gmail.com>
2020-09-14 16:09:35 -0700

    Added Name Header Feature--one bug

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,16 @@ export const fetchPosts = () => async dispatch => {
     dispatch({
         type: 'FETCH_POSTS',
         payload: response.data
-    })
+    });
+}
+
+export const fetchUser = (id) => async dispatch => { //input id as parameter for user's id.
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
 }
 
 

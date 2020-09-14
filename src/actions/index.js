101e6059dc2b77627a31896aa3d3dc90869b8ed6
Haz-git <zhouharry1@gmail.com>
2020-09-14 16:25:23 -0700

    Added Memoize from Lodash to prevent re-fetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 
@@ -10,14 +11,17 @@ export const fetchPosts = () => async dispatch => {
     });
 }
 
-export const fetchUser = (id) => async dispatch => { //input id as parameter for user's id.
-    const response = await jsonPlaceholder.get(`/users/${id}`);
+export const fetchUser = (id) => dispatch => { //input id as parameter for user's id.
+    _fetchUser(id, dispatch);
+}
 
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
     });
-}
+})
 
 
 
